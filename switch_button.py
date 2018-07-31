ec37e3a1025416ee215ee6f788adb6cf6f936ccb
unknown <you@example.com>
2018-07-31 11:40:04 +0800

    change main

--- switch_button.py
+++ switch_button.py
@@ -13,11 +13,10 @@ sta_if = network.WLAN(network.STA_IF)
 
 RESET_PIN = 4
 LED_PIN = 5
-RELAY_PIN_1 = 12
-RELAY_PIN_2 = 13
-RELAY_PIN_3 = 15
-RELAY_PIN_4 = 14
-botton1_pin= 2
+RELAY_PIN_1 = 14
+RELAY_PIN_2 = 12
+RELAY_PIN_3 = 13
+RELAY_PIN_4 = 15
 
 
 
@@ -36,10 +35,11 @@ CONFIG_DATA_SCHEMA = """
 """
 
 BASE_TOPIC = 'HA/switch/piliboard/' + DEVICE_NAME
+
+
+
+
 AVAILABILITY_TOPIC = BASE_TOPIC + "/availability"
-
-
-#设置对应设备的Command和State地址
 
 COMMAND_TOPIC1 = BASE_TOPIC+"1" + "/command"
 STATE_TOPIC1 = BASE_TOPIC+"1" + "/state"
@@ -50,16 +50,10 @@ STATE_TOPIC3 = BASE_TOPIC+"3" + "/state"
 COMMAND_TOPIC4 = BASE_TOPIC+"4" + "/command"
 STATE_TOPIC4 = BASE_TOPIC+"4" + "/state"
 
-
-
-
-
-#设置自动发现地址
 CONFIG_TOPIC1 = 'homeassistant/switch/piliboard/' + DEVICE_NAME+"1"  + '/config'
 CONFIG_TOPIC2 = 'homeassistant/switch/piliboard/' + DEVICE_NAME+"2"  + '/config'
 CONFIG_TOPIC3 = 'homeassistant/switch/piliboard/' + DEVICE_NAME+"3"  + '/config'
 CONFIG_TOPIC4 = 'homeassistant/switch/piliboard/' + DEVICE_NAME+"4"  + '/config'
-
 
 
 CONFIG_DATA1 = CONFIG_DATA_SCHEMA % (DEVICE_NAME+"1",
@@ -72,19 +66,18 @@ CONFIG_DATA2 = CONFIG_DATA_SCHEMA % (DEVICE_NAME+"2" ,
                                     STATE_TOPIC2,
                                     AVAILABILITY_TOPIC,
                                     )
+
 CONFIG_DATA3 = CONFIG_DATA_SCHEMA % (DEVICE_NAME+"3" ,
                                     COMMAND_TOPIC3,
                                     STATE_TOPIC3,
                                     AVAILABILITY_TOPIC,
                                     )
+
 CONFIG_DATA4 = CONFIG_DATA_SCHEMA % (DEVICE_NAME+"4",
                                     COMMAND_TOPIC4,
                                     STATE_TOPIC4,
                                     AVAILABILITY_TOPIC,
                                     )
-
-
-
 
 
 MqttClient = None
@@ -94,7 +87,6 @@ RELAY_1= Switch( led_num=RELAY_PIN_1)
 RELAY_2= Switch( led_num=RELAY_PIN_2)
 RELAY_3= Switch( led_num=RELAY_PIN_3)
 RELAY_4= Switch( led_num=RELAY_PIN_4)
-botton = machine.Pin(botton1_pin, machine.Pin.IN,machine.Pin.PULL_UP)
 
 
 def mqtt_start():
@@ -104,7 +96,6 @@ def mqtt_start():
         print("mqtt connected: subto {b}".format(b=COMMAND_TOPIC2))
         print("mqtt connected: subto {b}".format(b=COMMAND_TOPIC3))
         print("mqtt connected: subto {b}".format(b=COMMAND_TOPIC4))
-
         
         MqttClient.publish( CONFIG_TOPIC1, CONFIG_DATA1.encode(), retain=True)
         MqttClient.publish( CONFIG_TOPIC2, CONFIG_DATA2.encode(), retain=True)
@@ -149,7 +140,6 @@ def change_stat(msg,relay,topic):
     MqttClient.publish( topic, relay._state.encode(), retain=True )
 
 
-
 def mqtt_init(conf):
     global MqttClient
 
@@ -168,19 +158,16 @@ def mqtt_init(conf):
     MqttClient.set_last_will( AVAILABILITY_TOPIC, b"offline", retain=True)
     return True
 
+
 def toggle():
-    print('pressed')
+    print("pressed")
     
-def toggle1(v):
-    RELAY_1.toggle()
-    MqttClient.publish( STATE_TOPIC1, RELAY_1._state.encode(), retain=True )
-    print('pressed A')
-    
+
 
 
 def start():
     irq = IrqLongpressReset(pin_no=RESET_PIN, led_pin_no=LED_PIN, main_module=PROGRAM_NAME, press_action=toggle, device_name=DEVICE_NAME)
-    botton.irq(trigger=machine.Pin.IRQ_FALLING, handler=toggle1)
+
     last_try_time1 = 0
     last_try_time2 = 0
     last_resp_time = utime.time()
@@ -193,7 +180,7 @@ def start():
     else:
         print("mqtt_init failed")
         return
-    
+
 
     while True :
         if not sta_if.isconnected():
